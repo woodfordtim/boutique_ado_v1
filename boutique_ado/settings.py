f66e87100a6ad51ece376febd1cb082377e8f4d0
woodfordtim <woodfordtim@gmail.com>
2020-10-03 21:06:32 +0000

    Deploy website, part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -118,12 +119,12 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+ }
 
 
 # Password validation
